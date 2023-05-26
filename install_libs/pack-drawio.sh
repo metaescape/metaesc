65bf7fc4125095831754e4d5bbc9368f3ea4b036
mE <metaescape@foxmail.com>
2023-05-26 11:15:01 +0800

    drawio 以 deb 方式安装后如何卸载的困惑

--- install_libs/pack-drawio.sh
+++ install_libs/pack-drawio.sh
@@ -11,6 +11,7 @@ function install_drawio() {
 		print_info "使用 snap 安装有许多 bug(2023.5)"
 		print_info "到 https://github.com/jgraph/drawio-desktop/releases 手动下载 deb: "
 		print_notice "将 deb 放置在 /data/installer 下"
+		print_notice "目前还不清楚如何卸载这个 deb 包, 因此之考虑覆盖安装"
 		sudo dpkg -i /data/installer/drawio-amd64-21.2.8.deb
 		# sudo snap install drawio
 		print_success "drawio 安装完毕, 无需额外配置，直接系统启动 drawio"
