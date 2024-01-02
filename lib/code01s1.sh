309e0b06a2f3feedda16f956896cb772631aebfe
mE <metaescape@foxmail.com>
2024-01-02 16:29:49 +0800

    仅保持 metaesc 布局和默认布局

--- lib/code01s1.sh
+++ lib/code01s1.sh
@@ -3,4 +3,4 @@ xmodmap ~/metaesc/lib/code01s1.xmodmap
 sleep 0.2
 killall xcape
 sleep 0.2
-xcape -e 'Alt_L=Escape;Control_L=Control_R|c;Shift_L=space;Super_L=Control_R|Alt_L|Shift_R|l;Super_R=Control_R|Alt_L|Shift_R|i;Hyper_R=Control_R|Alt_L|f' -t 240
+xcape -e 'Alt_L=Escape;Control_L=Control_R|c;Shift_L=space;Super_L=Cancel;Super_R=Redo;Hyper_R=Control_R|Alt_L|f' -t 240
--- lib/code01s3.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-xmodmap ~/myconf/keyboards/swap_cap2ctrl.xmodmap
-sleep 0.2
-killall xcape
-sleep 0.2
