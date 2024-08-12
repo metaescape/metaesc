f720ef0bba0d424d5d81e5f3a14dadf3b045545d
mE <metaescape@foxmail.com>
2024-08-12 17:03:36 +0800

    rename videoio project to screenX

--- lib/screenx.sh
+++ lib/screenx.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # 查找 videoio/main.py 的进程
-PIDS=$(pgrep -f "/data/codes/ranger/videoio/main.py")
+PIDS=$(pgrep -f "/data/codes/ranger/screenX/main.py")
 
 if [ -n "$PIDS" ]; then
   # 如果发现已有进程，则杀掉所有相关进程
   kill -9 $PIDS
 else
   # 否则启动新进程
-  /home/pipz/miniconda3/envs/zshot/bin/python /data/codes/ranger/videoio/main.py -f &
+  /home/pipz/miniconda3/envs/zshot/bin/python /data/codes/ranger/screenX/main.py -f &
 fi
