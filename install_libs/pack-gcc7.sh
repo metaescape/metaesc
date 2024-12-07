2573859a32f56e34baa9d012b700e03603900bd7
mE <metaescape@foxmail.com>
2024-12-07 11:18:47 +0800

    添加 gcc7 安装脚本，并更新 gcc8 脚本以支持 gcc7 和 gcc13 的选择

--- install_libs/pack-gcc7.sh
+++ install_libs/pack-gcc7.sh
@@ -8,14 +8,14 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/../user_env.sh
 function install_foliate() {
 	local distro
 	distro=$(whichdistro)
-	print_info "ubuntu 22.04 后需要手动添加 gcc8 的源，主要是 cuda/nvcc 10.2 之前版本需要"
+	print_info "ubuntu 22.04 后需要手动添加 gcc8 的源，主要是 cuda/nvcc 10.0 之前版本需要"
 
 	if [[ $distro == "debian" ]]; then
-        echo "deb http://old-releases.ubuntu.com/ubuntu/ impish main restricted universe multiverse" | sudo tee /etc/apt/sources.list.d/gcc-8.list
+        echo "deb [arch=amd64] http://archive.ubuntu.com/ubuntu focal main universe" | sudo tee /etc/apt/sources.list.d/gcc-7.list
         sudo apt update
-        apt-cache policy g++-8
-        apt-cache show g++-8
-        sudo apt install g++-8
+        apt-cache policy g++-7
+        apt-cache show g++-7
+        sudo apt install g++-7
 	elif [[ $distro == "redhat" ]]; then
 		:
 	elif [[ $distro == "arch" ]]; then
@@ -26,10 +26,10 @@ function install_foliate() {
 function setup_foliate() {
 	print_info "通过 sudo update-alternatives --config gcc 查看不同 gcc 版本"
 	print_info "通过 sudo update-alternatives --config g++ 查看不同 g++ 版本"
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 2
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 2
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 2
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 2
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 1
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 1
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 1
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 1
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 1
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 1
 }
