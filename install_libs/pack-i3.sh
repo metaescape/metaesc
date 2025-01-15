52c3bf12b45bb89e8b759673540460e2608109a6
mE <metaescape@foxmail.com>
2025-01-15 23:24:50 +0800

    Install newest i3wm stable

--- install_libs/pack-i3.sh
+++ install_libs/pack-i3.sh
@@ -13,10 +13,17 @@ function install_i3() {
         print_info "例如 feh 设置壁纸，dunst 通知系统，wmctrl,xdotool,xcape 与改建相关"
         print_info "arandr compton 设置透明度"
         print_info "scrot 是截取动态图需要, emacs 中也会用到"
-	sudo apt update
-        pkgs="i3 feh dunst zenity wmctrl arandr compton xcape xdotool scrot gnome-screenshot"
+
+        /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
+        sudo apt install ./keyring.deb
+        echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+        sudo apt update
+        pkgs="feh dunst zenity wmctrl arandr compton xcape xdotool scrot gnome-screenshot"
         echo $pkgs | xargs sudo apt-get install -y
         print_success "i3 生态安装完毕"
+        sudo apt install i3
+
+        print_success "i3wm 最新稳定版安装完毕"
 	elif [[ $distro == "redhat" ]]; then
         :
 	elif [[ $distro == "arch" ]]; then
