79145f576e77383cca029c352ad7f67aa429d7b9
mE <metaescape@foxmail.com>
2025-02-10 12:42:41 +0800

    Fix i3 config to correctly execute clock_notify script and update backpages script to always start a new process

--- lib/backpages.sh
+++ lib/backpages.sh
@@ -5,7 +5,5 @@ PIDS=$(pgrep -f "/data/codes/ranger/backpages/main.py")
 if [ -n "$PIDS" ]; then
   # 如果发现已有进程，则杀掉所有相关进程
   kill -9 $PIDS
-else
-  # 否则启动新进程
-  /home/pipz/miniconda3/envs/usr/bin/python /data/codes/ranger/backpages/main.py &
 fi
+~/miniconda3/envs/usr/bin/python /data/codes/ranger/backpages/main.py &
