1501b9a2634baadb83a2fb15f90c67ea650130f9
mE <metaescape@foxmail.com>
2025-02-10 12:42:50 +0800

    Refactor screenx.sh to always start a new process for main.py, removing the conditional check for existing PIDs.

--- lib/screenx.sh
+++ lib/screenx.sh
@@ -5,7 +5,5 @@ PIDS=$(pgrep -f "/data/codes/ranger/screenX/main.py")
 if [ -n "$PIDS" ]; then
   # 如果发现已有进程，则杀掉所有相关进程
   kill -9 $PIDS
-else
-  # 否则启动新进程
-  /home/pipz/miniconda3/envs/zshot/bin/python /data/codes/ranger/screenX/main.py -f &
 fi
+/home/pipz/miniconda3/envs/zshot/bin/python /data/codes/ranger/screenX/main.py -f &
